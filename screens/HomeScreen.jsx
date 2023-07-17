c611ae1db41f4e541384332942e188112ad48125
adtimokhin <adtimokhin@gmail.com>
2023-07-17 17:16:04 +0600

    Added support for react navigation

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-lime-300">
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
