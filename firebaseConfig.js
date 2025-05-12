8bffe3ffcab035f9104b7dd4921b2ed9fbc0965e
Aleksandr Timokhin <adtimokhin@gmail.com>
2025-05-11 19:58:25 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -13,7 +13,7 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBoLAPqTP1UwrYGIZn8yBnmLcpJtTrJXZo",
+  apiKey: "",
   authDomain: "ayo-cc0f7.firebaseapp.com",
   projectId: "ayo-cc0f7",
   storageBucket: "ayo-cc0f7.appspot.com",
